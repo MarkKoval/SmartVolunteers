fcbc6228980f4a67192a4d6b6cb7e0e1e2045619
Mark Koval <marekmark22@gmail.com>
2023-12-08 00:41:59 +0200

    renamed:    credentials.json -> src/credentials.json 	renamed:    server.js -> src/server.js

--- src/server.js
+++ src/server.js
@@ -8,12 +8,10 @@ const path = require('path');
 const app = express();
 app.use(bodyParser.json());
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
-// Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/build/index.html'));
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
 // Load client secrets from a local file.
